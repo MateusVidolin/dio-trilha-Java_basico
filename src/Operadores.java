158b93c16ecd32c3923e741da52ee95ef28883d4
Mateus <mateusvidolin@outllok.com>
2023-03-03 21:31:07 -0300

    new class - metodos and operadores

---
+++ src/Operadores.java
@@ -0,0 +1,35 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //    Comando de atribuicao "="
+        int numero = 10;
+
+//    Aritimeticos: +, -, *, /, %
+        int a = 10;
+        int b = 10;
+        int total;
+
+        total = a + b;
+        System.out.println("total = " + total);
+
+/*        Operadores unarios
+          (+) deixa o valor positivo
+          (-) deixa o valor negativo
+          (++) incrementa o valor
+          (--) decrementa o valor
+          (!) Nega o valor
+ */
+//        Operador ternário
+        int c, d;
+        c = 6;
+        d = 7;
+        String resultado = c==d? "verdadeiro" : "falso";
+        System.out.println("resultado = " + resultado);
+
+//        operadores opercionais: ==, !=, >, <, >=, <=, .equals(compara dois objetos
+
+//      Operadores lógicos &&(and), ||(or)
+
+
+
+    }
+}
--- src/TiposDeVariaveis.java
+++ src/TiposDeVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposDeVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        final double pi = 3.1415;
+        String myName = "Mateus";
+        System.out.println("myName = " + myName + " pi = " + pi);
     }
 }
